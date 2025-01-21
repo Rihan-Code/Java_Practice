0a3a06301e0b6f3911f92667907cea8e427a3f6f
Ali <mohali@ciena.com>
2025-01-21 15:08:12 +0530

    feat: inheritance added

--- OopPrinciples/Encapsulation.java
+++ OopPrinciples/Encapsulation.java
@@ -3,6 +3,9 @@ package OopPrinciples;
 // Encapsulation is the process of wrapping code and data together into a single unit.
 // It is a protective barrier that keeps the data safe within the class and accessible only through the class's methods.
 // It is a way to achieve data hiding in Java because other class will not be able to access the data through the private data members.
+// Key features: 
+// 1. Data Hiding: Prevents unauthorized access to internal data by using private access modifiers
+// 2. Controlled Access: Provide access to data using public mehtods (eg. getters and setters)
 public class Encapsulation {
     private String name;
     private int age;
---
+++ OopPrinciples/Inheritance.java
@@ -0,0 +1,62 @@
+package OopPrinciples;
+
+// Inheritance is a mechanism where one class (child/derived/subclass) inherits properties and behavior (fields and methods) from another class (parent/base/superclass)
+// It allows code reuse and heirarchical classification
+// Key Features:
+// 1. Code resuability
+// 2. Extensibility
+// 3. Type of inheritance:
+//    i)   Single Inheritance
+//    ii)  Multilevel Inheritance
+//    iii) Hierarchical Inheritance
+
+// Parent Animal class
+class Animal {
+public void eat() {
+    System.out.println("This animal eats food.");
+}
+}
+
+// Child Animal class
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("This animal is a dog taht barks.");
+    }
+}
+
+// SHowing Multilevel Inheritance 
+// Parent Vehicle class
+class Vehicle {
+    public void start() {
+        System.out.println("Vehicle starts");
+    }
+}
+
+class Car extends Vehicle {
+    public void drive() {
+        System.out.println("Car Drives.");
+    }
+}
+
+class SportsCar extends Car {
+    public void race() {
+        System.out.println("Sports car races.");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        System.out.println("Inheritance using animal class: ");
+        d.eat(); // inheriting from the parent class
+        d.bark();
+
+        // multilevel inheritance 
+        System.out.println("Multilevel inheritance with vehicle example: ");
+        SportsCar ferari = new SportsCar();
+        ferari.start();
+        ferari.drive();
+        ferari.race();
+    }
+}
