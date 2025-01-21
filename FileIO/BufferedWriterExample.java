e58beacd739eed16477372dd46543bc0a994391e
Ali <mohali@ciena.com>
2025-01-21 17:25:57 +0530

    feat: buffered writer used for writing into the file

--- FileIO/BufferedWriterExample.java
+++ FileIO/BufferedWriterExample.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 
 public class BufferedWriterExample {
     public static void main(String[] args) {
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("FileIO/leanFileStream.txt", true))) {
-            writer.write("Appending ");
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("FileIO/learnFileStream.txt", true))) {
+            writer.write("Appending text using BufferedWriter.");
+            writer.newLine();
+            writer.write("BufferedWriter is efficient!");
+            System.out.println("Data appended to the file.");
+        } catch(IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- FileIO/WritingIntoFile.java
+++ FileIO/WritingIntoFile.java
@@ -3,22 +3,14 @@ import java.io.IOException;
 
 public class WritingIntoFile {
     public static void main(String[] args) {
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("FileIO/learnFileStream.txt");
+        // FileWriter writer = null;
+        try(FileWriter writer = new FileWriter("FileIO/learnFileStream.txt");) {
+            // writer = new FileWriter("FileIO/learnFileStream.txt");
             writer.write("Hello! I am super excited to write into a file using FileWriter class which is used for file streaming.");
             System.out.println("Data written to the file");
         } catch(IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }    
 }
